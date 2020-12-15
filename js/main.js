0193a1540b676bc7bbb814714fc6c5041ebab495
Ravencer <ravencer880@gmail.com>
2020-12-16 00:46:35 +0700

    fixed: diploma details

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 var menuButton = document.querySelector(".header__menu-button");
   menuButton.addEventListener('click', function(event){
     event.preventDefault();
-    document.querySelector(".header__container").classList.toggle('header__container--visible');
+    document.querySelector(".header__mobile-menu").classList.toggle('header__mobile-menu--visible');
     document.querySelector(".header__menu-button").classList.toggle('menu-button--visible');
     document.querySelector(".header__button").classList.toggle('header__button--visible');
-    document.querySelector(".header__logo").classList.toggle('header__logo--hidden');
     document.querySelector(".header__link-list").classList.toggle('header__link-list--visible');
     document.querySelector(".header__menu-close").classList.toggle('header__menu-close--visible');
   });
   var menuButton = document.querySelector(".header__menu-close");
   menuButton.addEventListener('click', function(event){
     event.preventDefault();
-    document.querySelector(".header__container").classList.toggle('header__container--visible');
+    document.querySelector(".header__mobile-menu").classList.toggle('header__mobile-menu--visible');
     document.querySelector(".header__menu-button").classList.toggle('menu-button--visible');
     document.querySelector(".header__button").classList.toggle('header__button--visible');
-    document.querySelector(".header__logo").classList.toggle('header__logo--hidden');
     document.querySelector(".header__link-list").classList.toggle('header__link-list--visible');
     document.querySelector(".header__menu-close").classList.toggle('header__menu-close--visible');
   });
