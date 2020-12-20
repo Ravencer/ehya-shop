39e476769985ac8192d8b6c8e1fe3eec0332abd3
Ravencer <ravencer880@gmail.com>
2020-12-20 17:36:30 +0700

    create: modal and responsive

--- js/main.js
+++ js/main.js
@@ -83,15 +83,59 @@ storiesSlider.on('reachBeginning', function(){
   storiesSlider.allowSlideNext = true;
   ButtonPrev.removeClass("stories__button-prev--active");
 });
+var modalButton = $('[data-toggle="modal"]');
+var closeModalButton = $(".modal__close");
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+$(document).on('keydown', function(e) {
+  if (e.keyCode == 27){
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+});
 
+function openModal (){
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+}
+
+function closeModal (event){
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+};
   $('.form').each(function(){
     $(this).validate({
       errorClass: "invalid",
       messages: {
+        name: {
+          required: "Пожалуйста, введите своё имя",
+          minlength: "Имя должно состоять хотя бы из 2 символов"
+        },
+        email: {
+          required: "Пожалуйста, введите корректный Email"
+        },
+        phone: {
+          required: "Введите номер в корректном формате",
+          minlength: "Введите номер в корректном формате"
+        },
         emailNews: {
           required: "Пожалуйста, введите корректный Email"
         }
       },
     });
-  })
+  });
+  $('.modal__input--tel').mask('+7 (ZZZ) ZZZ-ZZ-ZZ',{
+    translation: {
+      'Z': {
+        pattern: /[0-9]/, optional: true
+      }
+    }
+  });
 });
